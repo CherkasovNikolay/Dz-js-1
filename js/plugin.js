1920200e615e7f1987ce4500169a08f4034f7da7
CherkasovNikolay <mbikojika@gmail.com>
2017-04-14 17:55:55 +0300

    first commit

---
+++ js/plugin.js
@@ -0,0 +1,109 @@
+
+
+//DZ 1
+
+// var str = 'i am in the easycode',
+//   newStr = '';
+// for (let i = 0; i < str.length; i++) {
+//
+//   if (str[i-1]==' ' || i == 0 ){
+//     newStr += str[i].toUpperCase();
+//
+//   }else{
+//     newStr += str[i];
+//
+//   }
+//
+// }
+// console.log(newStr);
+
+//DZ 2
+
+// let str = 'tseb eht ma i',
+//     i = str.length,
+//     newStr='';
+//  while (i--){
+//    newStr +=str[i];
+//
+//  }
+// console.log(newStr);
+
+
+//   newStr='';
+// for (let i = 0,max = str.length; i < max; ++i){
+//   if(str[i] == 0){
+//     newStr+=str[];
+//   }else {
+//   }
+//
+//   console.log(newStr);
+// }
+
+
+//DZ 3
+//----------Факториал числа 10------------
+
+// let f=1;
+// for (let i = 10; i>0; i--){
+//   f *=i;
+// }
+// console.log(f);
+
+// DZ 4
+
+//---------------Считае до 10------------------
+
+
+// let str = 'Считаем до 10и: ';
+//
+//   for(let i = 0; i <=10; i++){
+//     str += (i == 10) ? i : i +', ';
+//
+//   }
+//
+//   console.log(str);
+// let str = 'Считаем до 10и: ';
+//   for (let i = 1; i<=10; ++i){
+//     if(i<=10) {
+//        str+= i + ', ';
+//     }
+//
+//   }
+// console.log(str);
+
+
+// DZ 5
+//
+// Убрать пробелы и сделать каждое слово в с верхнего регистра
+
+// var str = 'JavaScript is a pretty good language',
+//   newStr = '',
+// newStr2 = '';
+// for (let i = 0; i < str.length; i++) {
+//
+//   if (str[i-1]==' '|| i == 0) {
+//     newStr += str[i].toUpperCase();
+//
+//   }if (str[i]==' '){
+//     continue;
+//   }
+//   else{
+//     newStr += str[i]
+//   }
+//
+// }
+// console.log(newStr);
+
+//DZ 6
+//--------------Найти все нечетные числа от 1 до 15
+// for (var i = 0; i <= 15; ++i) {
+//
+//   if (i % 2 == 0) continue;
+//
+//   console.log(i);
+//
+// }
+
+
+
+
